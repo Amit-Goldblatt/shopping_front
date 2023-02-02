737418e6ee4dadc0b5264e6d5dc4e3d77474d8eb
amit goldblatt <amit@goldblatt.in>
2023-02-02 19:50:37 +0200

    test url

--- src/components/http.js
+++ src/components/http.js
@@ -1,12 +1,14 @@
 // create a url based on environment
 let BaseUrl = '';
-if (process.env._ && process.env._.indexOf("heroku") !== -1){
 
+if (process.env._ && process.env._.indexOf("heroku") !== -1){
+console.log("heroku")
 
 BaseUrl = 'https://backshoping.herokuapp.com/';
 }
 else 
 {
+console.log("localhost")
 BaseUrl ='http://localhost:8000';
 }
 export {BaseUrl}
